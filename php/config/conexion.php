69aa476fe85d2d8903bcf78b60c01817043335b0
Sistolic <130786208+Sistolic@users.noreply.github.com>
2024-04-24 15:45:15 -0600

    Update

--- php/config/conexion.php
+++ php/config/conexion.php
@@ -1,11 +1,11 @@
 <?php
     session_start();
 
-    $host=$_ENV["MYSQLHOST"];
-    $user=$_ENV["MYSQLUSER"];
-    $password=$_ENV["MYSQL_ROOT_PASSWORD"];
-    $db_name=$_ENV["MYSQL_DATABASE"];
-    $port=$_ENV["MYSQLPORT"];
+    $host=$_ENV["DB_HOST"];
+    $user=$_ENV["DB_USER"];
+    $password=$_ENV["DB_PASSWORD"];
+    $db_name=$_ENV["DB_NAME"];
+    $port=$_ENV["DB_PORT"];
 
     $conexion=new mysqli_connect("$host", "$user", "$password", "$db_name", "$port");
     // $conexion->set_charset('utf8');
