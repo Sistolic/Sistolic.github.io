6d665e1f46f4978a0b8915d9a382274d03e1082c
Sistolic <130786208+Sistolic@users.noreply.github.com>
2024-04-26 13:13:29 -0600

    Update

--- php/config/sign_Up_Controller.php
+++ php/config/sign_Up_Controller.php
@@ -1,16 +1,17 @@
 <?php
     if(!empty($_POST["register"])) {
         if(empty($_POST["name"]) or empty($_POST["user"]) or empty($_POST["password"])) {
-            echo '<div class="alert">Uno de los campos esta vacio</div>';
+            echo '<div class="alert">Uno de los campos está vacío</div>';
         } else {
             $name = $_POST["name"];
             $user = $_POST["user"];
             $password = md5($_POST["password"]);
-            
-            $sql = $conexion->query(" insert into user(user, password, name) values('$user', '$password', '$name') ");
+
+            $sql = $conexion->query("INSERT INTO user(user, password, name) VALUES ('$user', '$password', '$name') ");
 
             if($sql == 1) {
-                header("location:../sign_In.php");
+                header("Location: ../sign_In.php");
+                exit();
             } else {
                 echo '<div class="alert">Error al registrar</div>';
             }
