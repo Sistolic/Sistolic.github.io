327d13f524829f6cbebc1b22715c1b34c6de235e
Sistolic <130786208+Sistolic@users.noreply.github.com>
2024-04-26 13:11:36 -0600

    Update

--- php/config/sign_In_Controller.php
+++ php/config/sign_In_Controller.php
@@ -1,18 +1,21 @@
 <?php
     if(!empty($_POST["login"])) {
         if(empty($_POST["user"]) or empty($_POST["password"])) {
-            echo '<div class="alert">Uno de los campos esta vacio</div>';
+            $message = '<div class="alert">Uno de los campos está vacío</div>';
         } else {
             $user = $_POST["user"];
             $password = md5($_POST["password"]);
-            $sql = $conexion->query(" select * from user where user = '$user' and password = '$password' ");
+            $sql = $conexion->query("SELECT * FROM user WHERE user = '$user' AND password = '$password'");
             
             if($datos = $sql->fetch_object()) {
                 $_SESSION['user'] = $user;
-                header("location:/index.php");
+                header("Location: /index.php");
+                exit();
             } else {
-                echo '<div class="alert">Acceso denegado</div>';
+                $message = '<div class="alert">Acceso denegado</div>';
             }
         }
     }
 ?>
+
+<?php if(isset($message)) echo $message; ?>
