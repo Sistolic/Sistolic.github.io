0af19d5ca3555bc433bf66e4785e7943eddadb2e
Sistolic <130786208+Sistolic@users.noreply.github.com>
2024-05-06 14:21:48 -0600

    Update

--- php/sign_In.php
+++ php/sign_In.php
@@ -34,7 +34,7 @@
         <input class="button" type="submit" value="Iniciar sesión" name="login">
         <div class="link">
           <p>¿No tienes cuenta? <a href="sign_Up.php">Registrate</a></p>
-          <p>Continuar sin cuenta. <a href="../index.html">Volver</a></p>
+          <p>Continuar sin cuenta. <a href="../index.php">Volver</a></p>
         </div>
       </form>
     </div>
