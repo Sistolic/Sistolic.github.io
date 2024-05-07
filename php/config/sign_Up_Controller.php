136f344c298915b1311fef167103ae47c45169a0
Sistolic <130786208+Sistolic@users.noreply.github.com>
2024-05-07 14:27:41 -0600

    Update

--- php/config/sign_Up_Controller.php
+++ php/config/sign_Up_Controller.php
@@ -1,14 +1,21 @@
 <?php
+    include 'conexion.php';
+
     if(!empty($_POST["register"])) {
         $name = $_POST["name"];
         $user = $_POST["user"];
         $password = md5($_POST["password"]);
 
-        $sql = $conexion->query("INSERT INTO user(user, password, name) VALUES ('$user', '$password', '$name') ");
-
-        if($sql == 1) {
-            echo '<script>window.location="../php/sign_In.php";</script>';
-            exit();
+        $repeat = $conexion->query(" SELECT * FROM user WHERE user = '$user' ");
+            
+        if($datos = $repeat->fetch_object()) {
+            echo '<p class="session-control duplicate" style="display: flex;">Usuario existente.</p>';
+        } else {
+            $sql = $conexion->query(" INSER INTO user(user, password, name) VALUES('$user', '$password', '$name') ");
+                
+            if($sql == 1) {
+                header("location:sign_In.php");
+            }
         }
     }
 ?>
