2915b6c97f6f20c188630c2b0f7b5b568cf781ab
Sistolic <130786208+Sistolic@users.noreply.github.com>
2024-05-27 16:07:34 -0600

    Update

--- php/sign_Up.php
+++ php/sign_Up.php
@@ -5,7 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Registrate</title>
     <link href="style/style.css" rel="stylesheet">
-    <link rel="icon" href="../logo.png" type="image/png">
+    <link rel="icon" href="/pwa/logo.png" type="image/png">
   </head>
   <body>
     <div class="container">
