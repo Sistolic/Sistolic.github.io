f00afea4029056a8f81cf1bba58eaa60ce6b6988
Sistolic <130786208+Sistolic@users.noreply.github.com>
2024-06-08 10:06:04 -0600

    Updat

--- script.js
+++ script.js
@@ -1,234 +1,221 @@
 function signUp() {
-  window.location.href = "php/sign_Up.php";
+    window.location.href = "php/sign_Up.php";
 }
+
 function signIn() {
-  window.location.href = "/";
+    window.location.href = "index.php";
 }
+
 function logOut() {
-  fetch('php/config/session.php')
-  .then(response => response.json())
-  .then(data => {
-    if(data.sesion_activa) {
-      if(window.confirm("Restaurar tareas / Salir")) {
-        userTasks();
-      } else {
-        saveToDB();
-        // deleteValue();
-        // window.location.href = "php/config/log_Out.php";
-      }
-    } else {
-      alert("No has iniciado sesión.");
-    }
-  })
-  .catch(error => {
-    console.error(error);
-  });
+    fetch('php/config/session.php')
+        .then(response => response.json())
+        .then(data => {
+            if (data.sesion_activa) {
+                if (window.confirm("Restaurar tareas / Salir")) {
+                    userTasks();
+                } else {
+                    saveToDB();
+                    deleteValue();
+                    // window.location.href = "php/config/log_Out.php";
+                }
+            } else {
+                alert("No has iniciado sesión.");
+            }
+        })
+        .catch(error => {
+            console.error('Error al verificar la sesión:', error);
+        });
 }
 
 const done = document.querySelector('.done');
 
 function showDone() {
-  done.style.display = 'flex';
+    done.style.display = 'flex';
 }
+
 function hideDone() {
-  done.style.display = 'none';
+    done.style.display = 'none';
 }
-//----------------------------------------------\\
+
 var list = document.getElementById('tasksList');
 var input = document.getElementById('taskInput');
 
 function addFromStorage() {
-  const storedTasks = localStorage.getItem('tasks');
+    const storedTasks = localStorage.getItem('tasks');
 
-  if (storedTasks) {
-    const tasks = JSON.parse(storedTasks);
-    tasks.forEach(function(task) {
-      if (task.isChecked) {
-        addToDone(task.content);
-      } else {
-        addList(task.content, task.isChecked);
-      }
-    });
-  }
+    if (storedTasks) {
+        const tasks = JSON.parse(storedTasks);
+        tasks.forEach(task => {
+            if (task.isChecked) {
+                addToDone(task.content);
+            } else {
+                addList(task.content, task.isChecked);
+            }
+        });
+    }
 }
 
 addFromStorage();
 
-document.getElementById('addButton').addEventListener('click', function() {
-  if (input.value !== '') {
-    addList(input.value);
-    addStorage(input.value, false);
-    input.value = '';
-    input.placeholder = 'Ingresa una tarea';
-  } else {
-    alert("Debes agregar una tarea");
-  }
+document.getElementById('addButton').addEventListener('click', function () {
+    if (input.value !== '') {
+        addList(input.value);
+        addStorage(input.value, false);
+        input.value = '';
+        input.placeholder = 'Ingresa una tarea';
+    } else {
+        alert("Debes agregar una tarea");
+    }
 });
 
 function addList(content) {
-  const task = document.createElement('li');
+    const task = document.createElement('li');
 
-  const checkButton = document.createElement('button');
-  checkButton.classList.add('check-button');
-  checkButton.addEventListener('click', function() {
-    addToDone(content);
-    list.removeChild(task);
-    updateStorage(content, content, true);
-  });
+    const checkButton = document.createElement('button');
+    checkButton.classList.add('check-button');
+    checkButton.addEventListener('click', function () {
+        addToDone(content);
+        list.removeChild(task);
+        updateStorage(content, content, true);
+    });
 
-  const text = document.createTextNode(content);
-  task.appendChild(checkButton);
-  task.appendChild(text);
+    const text = document.createTextNode(content);
+    task.appendChild(checkButton);
+    task.appendChild(text);
 
-  const editButton = document.createElement('button');
-  editButton.classList.add('edit-button');
-  editButton.addEventListener('click', function() {
-    const newContent = prompt('Editar tarea:', text.nodeValue.trim());
-    
-    if (newContent !== '') {
-      text.nodeValue = newContent.trim();
-      updateStorage(content, newContent.trim(), false);
-      location.reload();
-    }
-  });
+    const editButton = document.createElement('button');
+    editButton.classList.add('edit-button');
+    editButton.addEventListener('click', function () {
+        const newContent = prompt('Editar tarea:', text.nodeValue.trim());
 
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('delete-button');
-  deleteButton.addEventListener('click', function() {
-    if (window.confirm("¿Eliminar?")) {
-      list.removeChild(task);
-      removeFromStorage(content);
-    }
-  });
+        if (newContent !== '') {
+            text.nodeValue = newContent.trim();
+            updateStorage(content, newContent.trim(), false);
+            location.reload();
+        }
+    });
 
-  task.appendChild(deleteButton);
-  task.appendChild(editButton);
-  list.appendChild(task);
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', function () {
+        if (window.confirm("¿Eliminar?")) {
+            list.removeChild(task);
+            removeFromStorage(content);
+        }
+    });
+
+    task.appendChild(deleteButton);
+    task.appendChild(editButton);
+    list.appendChild(task);
 }
 
 function addStorage(content, isChecked) {
-  var key = 'tasks';
-  const tasks = JSON.parse(localStorage.getItem(key)) || [];
-  tasks.push({ content: content, isChecked: isChecked});
-  localStorage.setItem(key, JSON.stringify(tasks));
+    const key = 'tasks';
+    const tasks = JSON.parse(localStorage.getItem(key)) || [];
+    tasks.push({ content: content, isChecked: isChecked });
+    localStorage.setItem(key, JSON.stringify(tasks));
 }
 
 function removeFromStorage(content) {
-  const storedTasks = localStorage.getItem('tasks');
-  if (storedTasks) {
-    const tasks = JSON.parse(storedTasks);
-    const updatedTasks = tasks.filter(task => task.content !== content);
-    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-  }
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+        const tasks = JSON.parse(storedTasks);
+        const updatedTasks = tasks.filter(task => task.content !== content);
+        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    }
 }
 
 function addToDone(content) {
-  const done = document.querySelector('.done');
-  const task = document.createElement('li');
-  const text = document.createTextNode(content);
+    const done = document.querySelector('.done');
+    const task = document.createElement('li');
+    const text = document.createTextNode(content);
 
-  const undoButton = document.createElement('button');
-  undoButton.classList.add('undo-button');
-  undoButton.addEventListener('click', function() {
-    addList(content, false);
-    updateStorage(content, content, false);
-    done.removeChild(task);
-  });
+    const undoButton = document.createElement('button');
+    undoButton.classList.add('undo-button');
+    undoButton.addEventListener('click', function () {
+        addList(content, false);
+        updateStorage(content, content, false);
+        done.removeChild(task);
+    });
 
-  const deleteButton = document.createElement('button');
-  deleteButton.classList.add('delete-button');
-  deleteButton.addEventListener('click', function() {
-    if (window.confirm("¿Eliminar?")) {
-      done.removeChild(task);
-      removeFromStorage(content);
-    }
-  });
-  
-  task.appendChild(text);
+    const deleteButton = document.createElement('button');
+    deleteButton.classList.add('delete-button');
+    deleteButton.addEventListener('click', function () {
+        if (window.confirm("¿Eliminar?")) {
+            done.removeChild(task);
+            removeFromStorage(content);
+        }
+    });
 
-  task.appendChild(deleteButton);
-  task.appendChild(undoButton);
-  done.appendChild(task);
+    task.appendChild(text);
+    task.appendChild(deleteButton);
+    task.appendChild(undoButton);
+    done.appendChild(task);
 }
 
 function updateStorage(oldContent, newContent, isChecked) {
-  const storedTasks = localStorage.getItem('tasks');
-  if (storedTasks) {
-    const tasks = JSON.parse(storedTasks);
-    const updatedTasks = tasks.map(task => {
-      if (task.content === oldContent) {
-        return { content: newContent, isChecked: isChecked };
-      }
-      return task;
-    });
-    localStorage.setItem('tasks', JSON.stringify(updatedTasks));
-  }
+    const storedTasks = localStorage.getItem('tasks');
+    if (storedTasks) {
+        const tasks = JSON.parse(storedTasks);
+        const updatedTasks = tasks.map(task => {
+            if (task.content === oldContent) {
+                return { content: newContent, isChecked: isChecked };
+            }
+            return task;
+        });
+        localStorage.setItem('tasks', JSON.stringify(updatedTasks));
+    }
 }
 
 function deleteValue() {
-  var tasks = JSON.parse(localStorage.getItem('tasks'));
-
-  if (tasks) {
-    tasks = [];
-    localStorage.setItem('tasks', JSON.stringify(tasks));
-  }
+    localStorage.setItem('tasks', JSON.stringify([]));
 }
 
 function saveToDB() {
-  var tasksByUser = JSON.parse(localStorage.getItem('tasks'));
+    const tasksByUser = JSON.parse(localStorage.getItem('tasks'));
 
-  try {
-    if (tasksByUser.length !== 0) {
-      tasksByUser.map(task => {
-        var tasksToDB = {
-          task: task.content,
-          isChecked: task.isChecked
-        }
-        
-        fetch('php/config/config.php', {
-          method: 'POST',
-          headers: {
-            'Content-Type': 'application/json',
-          },
-          body: JSON.stringify(tasksToDB),
-        })
-        .then(response => response.text())
-        .then(data => {
-          console.log(data);
-        })
-        .catch(error => {
-          console.error('Error al enviar datos al servidor:', error);
+    if (tasksByUser && tasksByUser.length !== 0) {
+        tasksByUser.forEach(task => {
+            const tasksToDB = {
+                task: task.content,
+                isChecked: task.isChecked
+            };
+
+            fetch('php/config/config.php', {
+                method: 'POST',
+                headers: {
+                    'Content-Type': 'application/json',
+                },
+                body: JSON.stringify(tasksToDB),
+            })
+                .then(response => response.json())
+                .then(data => {
+                    console.log(data.message);
+                })
+                .catch(error => {
+                    console.error('Error al enviar datos al servidor:', error);
+                });
         });
-      });
+    } else {
+        console.log("No hay tareas que agregar.");
     }
-  } catch(e) {
-    console.log("No hay tareas que agregar.")
-  }
 }
 
 function userTasks() {
-  fetch('php/config/config.php')
-  .then(response => response.json())
-  .then(data => {
-    if (!data.empty) {
-      data.forEach(item => {
-        intBool = bool(item.is_checked);
-        addStorage(item.task, intBool);
-      });
-      location.reload();
-    } else {
-      alert("No has guardado tareas.")
-    }
-  })
-  .catch(error => {
-    console.error("Error al obtener los datos", error);
-  });
-}
-
-function bool(int) {
-  if (int == 1) {
-    return true;
-  } else if (int == 0) {
-    return false;
-  }
+    fetch('php/config/config.php')
+        .then(response => response.json())
+        .then(data => {
+            if (!data.empty) {
+                data.forEach(item => {
+                    const isChecked = Boolean(item.is_checked);
+                    addStorage(item.task, isChecked);
+                });
+                location.reload();
+            } else {
+                alert("No has guardado tareas.");
+            }
+        })
+        .catch(error => {
+            console.error("Error al obtener los datos", error);
+        });
 }
